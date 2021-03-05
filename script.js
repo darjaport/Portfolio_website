5ad5c21a365a2ba1df1f5cc2c88af6481798269f
Darja Port <darja.port@gmail.com>
2021-03-05 16:36:04 +0100

    edit mobile nav

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const homePage = document.querySelector(".home");
 const btnMoreBg = document.querySelector(".btn__learn-more--bg-screen");
 const btnMoreSm = document.querySelector(".btn__learn-more--sm-screen");
 
+const navigation = document.querySelector(".navigation");
 const navLinks = document.querySelector(".navigation ul");
 
 // Big screen pages
@@ -50,4 +51,28 @@ navLinks.addEventListener("click", (e) => {
     const link = e.target.getAttribute("href");
     document.querySelector(link).scrollIntoView({ behavior: "smooth" });
   }
+});
+
+const hoverNavLink = function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".navigation").querySelectorAll(".nav__link");
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+  }
+};
+
+navigation.addEventListener("mouseover", hoverNavLink.bind(0.5));
+navigation.addEventListener("mouseout", hoverNavLink.bind(1));
+
+navigation.addEventListener("click", (e) => {
+  const clicked = e.target.closest(".navigation__item");
+  const siblings = document.querySelectorAll(".navigation__item");
+
+  siblings.forEach((el) => {
+    el.classList.remove("avtive__nav-link");
+  });
+  clicked.classList.add("avtive__nav-link");
 });
